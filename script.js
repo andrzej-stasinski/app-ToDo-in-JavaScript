e3946038c28520d866c2a606a96b41e5adcc0eed
Andrzej Stasinski <andis_as@wp.pl>
2020-12-28 16:08:58 +0100

    show tasks - tasks as array with object

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
 'use strict'
 
+let tasks = [
+    {name: 'Wyrzucić śmieci'}
+]
+
+const appendArray = function(array, container) {
+    array.forEach(el => {
+        container.appendChild(el)
+    });
+}
 const renderInput = function() {
     const input = document.createElement('input')
     return input
 }
 
+const renderTask = function(task) {
+    const container = document.createElement('div')
+    console.log(task.name)
+    container.textContent = task.name
+    console.log(container)
+    return container  
+}
+
+const renderTaskList = function() {
+    const container = document.createElement('div')
+    const tasksElements = tasks.map(task => {
+        console.log(task)
+        return renderTask(task)
+    })
+    console.log(tasksElements)
+    appendArray(tasksElements, container)
+    return container     
+}
+
 const render = function() {
-    const div = document.createElement('div')
-    div.textContent = 'div - my Div'
+    const container = document.createElement('div')
+    container.textContent = 'div - my Div'
 
     const input = renderInput()
-    div.appendChild(input)
-    
-    return div    
+    const renderTasksElements = renderTaskList()
+
+    container.appendChild(input)
+    container.appendChild(renderTasksElements)
+
+    return container    
 }
 
 const init = function(selector) {
     const container = document.querySelector(selector)
+    if(!container) return console.log('Selector not exists')
     const app = render()
     container.appendChild(app)
 }
 
+// init()
 init('.root')
 
 
