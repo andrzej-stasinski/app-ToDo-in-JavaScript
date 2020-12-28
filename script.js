f115c0b4da764a13da0913b1832a596ffd9d302a
Andrzej Stasinski <andis_as@wp.pl>
2020-12-28 15:37:13 +0100

    render div with input

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 'use strict'
-console.log('script')
+
+const renderInput = function() {
+    const input = document.createElement('input')
+    return input
+}
+
+const render = function() {
+    const div = document.createElement('div')
+    div.textContent = 'div - my Div'
+
+    const input = renderInput()
+    div.appendChild(input)
+    
+    return div    
+}
+
+const init = function(selector) {
+    const container = document.querySelector(selector)
+    const app = render()
+    container.appendChild(app)
+}
+
+init('.root')
+
+
+
 
 
 
